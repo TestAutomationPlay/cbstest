a3d8006a714ac46ca988f70d3d08f908a731ef56
Ramkumar Ranganathan <ramkumarranganathan@Ramkumars-MacBook-Air.local>
2023-10-10 13:23:10 +1100

    feat: updated readme and step definition file.

--- cba.step.js
+++ cba.step.js
@@ -12,7 +12,7 @@ Then('the product to shopping cart and verify in the cart of the same product do
 	let  cartItems = page.getByLabel(‘get Cart item’).getbyList(‘items’)
 	let  cartItemsPrice = page.getByLabel(‘get Cart item’).getByList(‘items’).getByLabel(‘Price’)
 
-  expect(cartItems.find(item=>{item===Dove})).toBeTruthy()
+  expect(cartItems.find(item=>{item==='Dove'})).toBeTruthy()
   expect(cartItems.find(itemPrice=>{item===33.33})).toBeTruthy()
 })
 
@@ -22,7 +22,7 @@ Page.getByLabel(‘add to the cart’).click()
 
 })
 
-Then(‘verify the product in the cart and its unit price changes to the units added’,async function(url){
+Then(‘verify the product in the cart and its unit price changes to the unit added’,async function(url){
 
 	let  cartItems = page.getByLabel(‘get Cart item’).getbyList(‘items’)
 	let  cartItemsPrice = page.getByLabel(‘get Cart item’).getByList(‘items’).getByLabel(‘Price’)
@@ -34,7 +34,8 @@ Then(‘verify the product in the cart and its unit price changes to the units a
 
 
 Then(‘remove a product from the cart and verify the price',async function(url){
-Page.getByLabel(‘remove to the cart’).click()
+  
+  Page.getByLabel(‘remove to the cart’).click()
 
   let  cartItems = page.getByLabel(‘get Cart item’).getbyList(‘items’)
 	let  cartItemsPrice = page.getByLabel(‘get Cart item’).getByList(‘items’).getByLabel(‘Price’)
